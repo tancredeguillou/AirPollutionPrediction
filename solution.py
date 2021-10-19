94491a4535ca67182ca79cdac56ca552f02fc5c2
louancillon <lou.ancillon@epfl.ch>
2021-10-19 17:01:52 +0200

    changes

--- solution.py
+++ solution.py
@@ -28,12 +28,18 @@ gpflow.config.set_default_summary_fmt("notebook")
 f64 = gpflow.utilities.to_default_float
 
 
+
 # Set `EXTENDED_EVALUATION` to `True` in order to visualize your predictions.
-EXTENDED_EVALUATION = True
+EXTENDED_EVALUATION = False
 EVALUATION_GRID_POINTS = 300  # Number of grid points used in extended evaluation
 EVALUATION_GRID_POINTS_3D = 50  # Number of points displayed in 3D during evaluation
 
 
+# Cost function constants
+THRESHOLD = 35.5
+COST_W_NORMAL = 1.0
+COST_W_OVERPREDICT = 5.0
+COST_W_THRESHOLD = 20.0
 
 def eprint(*args, **kwargs):
     print(*args, file=sys.stderr, **kwargs)
@@ -48,54 +54,6 @@ def timeit(function):
         return retval
 
     return wrapper
-
-# Cost function constants
-THRESHOLD = 35.5
-COST_W_NORMAL = 1.0
-COST_W_OVERPREDICT = 5.0
-COST_W_THRESHOLD = 20.0
-
-
-def cost_function(y_true: np.ndarray, y_predicted: np.ndarray) -> float:
-    """
-    Calculates the cost of a set of predictions.
-
-    :param y_true: Ground truth pollution levels as a 1d NumPy float array
-    :param y_predicted: Predicted pollution levels as a 1d NumPy float array
-    :return: Total cost of all predictions as a single float
-    """
-    assert y_true.ndim == 1 and y_predicted.ndim == 1 and y_true.shape == y_predicted.shape
-
-    # Unweighted cost
-    cost = (y_true - y_predicted) ** 2
-    weights = np.zeros_like(cost)
-
-    # Case i): overprediction
-    mask_1 = y_predicted > y_true
-    weights[mask_1] = COST_W_OVERPREDICT
-
-    # Case ii): true is above threshold, prediction below
-    mask_2 = (y_true >= THRESHOLD) & (y_predicted < THRESHOLD)
-    weights[mask_2] = COST_W_THRESHOLD
-
-    # Case iii): everything else
-    mask_3 = ~(mask_1 | mask_2)
-    weights[mask_3] = COST_W_NORMAL
-
-    # Weigh the cost and return the average
-    return np.mean(cost * weights)
-
-"""
-Fill in the methods of the Model. Please do not change the given methods for the checker script to work.
-You can add new methods, and make changes. The checker script performs:
-
-
-    M = Model()
-    M.fit_model(train_x,train_y)
-    prediction = M.predict(test_x)
-
-It uses predictions to compare to the ground truth using the cost_function above.
-"""
 
 def run_adam(model, iterations, train_dataset, minibatch_size = 100):
     """
@@ -129,9 +87,10 @@ def select_inducing_variables(n, X):
     random_indices = np.random.choice(X_left.shape[0], size=n)
     return np.concatenate([X[mask], X_left[random_indices]], axis=0)
 
-kernels = { 
+
+kernels = {
     "exp"       : gpflow.kernels.Exponential,
-    "matern12"  : gpflow.kernels.Matern12, 
+    "matern12"  : gpflow.kernels.Matern12,
     "matern32"  : gpflow.kernels.Matern32,
     "matern52"  : gpflow.kernels.Matern52,
     "rbf"       : gpflow.kernels.SquaredExponential,
@@ -141,10 +100,20 @@ kernels = {
     "poly"      : gpflow.kernels.Polynomial,
     "arccosine" : gpflow.kernels.ArcCosine,
     "coregion"  : gpflow.kernels.Coregion
-} 
+}
 
-class Model():
+class Model(object):
+    """
+    Model for this task.
+    You need to implement the fit_model and predict methods
+    without changing their signatures, but are allowed to create additional methods.
+    """
+
     def __init__(self):
+        """
+        Initialize your model here.
+        We already provide a random number generator for reproducibility.
+        """
         params = Parameters()
         self.kernel = kernels[params.k]
         self.inducing_variables = params.inducing_variables
@@ -153,8 +122,34 @@ class Model():
         self.predict_func = params.predict_func
         self.model = None
 
-    @timeit
-    def fit_model(self, train_x, train_y):
+
+    def predict(self, x: np.ndarray) -> typing.Tuple[np.ndarray, np.ndarray, np.ndarray]:
+        """
+        Predict the pollution concentration for a given set of locations.
+        :param x: Locations as a 2d NumPy float array of shape (NUM_SAMPLES, 2)
+        :return:
+            Tuple of three 1d NumPy float arrays, each of shape (NUM_SAMPLES,),
+            containing your predictions, the GP posterior mean, and the GP posterior stddev (in that order)
+        """
+
+        # TODO: Use your GP to estimate the posterior mean and stddev for each location here
+        gp_mean = np.zeros(x.shape[0], dtype=float)
+        gp_std = np.zeros(x.shape[0], dtype=float)
+
+        # TODO: Use the GP posterior to form your predictions here
+        predictions = self.model.predict_y(x)
+        mean, var = self.model.predict_f(x)
+
+        return tf.reshape(predictions[:][0], [-1]), tf.reshape(predictions[:][1], [-1]), tf.reshape(var, [-1])
+
+
+    def fit_model(self, train_x: np.ndarray, train_y: np.ndarray):
+        """
+        Fit your model on the given training data.
+        :param train_x: Training features as a 2d NumPy float array of shape (NUM_SAMPLES, 2)
+        :param train_y: Training pollution concentrations as a 1d NumPy float array of shape (NUM_SAMPLES,)
+        """
+
         train_y = np.expand_dims(train_y, axis=1) # check si necessaire
         train_dataset = tf.data.Dataset.from_tensor_slices((train_x, train_y)).repeat().shuffle(train_x.shape[0])
 
@@ -173,17 +168,43 @@ class Model():
         _ = run_adam(self.model, ci_niter(self.adam_iterations), train_dataset, self.minibatch_size)
 
 
-    def predict(self, test_x):
-        mean, var = self.model.predict_f(test_x)
-        return mean, var
+def cost_function(y_true: np.ndarray, y_predicted: np.ndarray) -> float:
+    """
+    Calculates the cost of a set of predictions.
+
+    :param y_true: Ground truth pollution levels as a 1d NumPy float array
+    :param y_predicted: Predicted pollution levels as a 1d NumPy float array
+    :return: Total cost of all predictions as a single float
+    """
+    print(y_true.shape)
+    print(y_predicted.shape)
+    assert y_true.ndim == 1 and y_predicted.ndim == 1 and y_true.shape == y_predicted.shape
+
+    # Unweighted cost
+    cost = (y_true - y_predicted) ** 2
+    weights = np.zeros_like(cost)
+
+    # Case i): overprediction
+    mask_1 = y_predicted > y_true
+    weights[mask_1] = COST_W_OVERPREDICT
+
+    # Case ii): true is above threshold, prediction below
+    mask_2 = (y_true >= THRESHOLD) & (y_predicted < THRESHOLD)
+    weights[mask_2] = COST_W_THRESHOLD
+
+    # Case iii): everything else
+    mask_3 = ~(mask_1 | mask_2)
+    weights[mask_3] = COST_W_NORMAL
+
+    # Weigh the cost and return the average
+    return np.mean(cost * weights)
 
 class Parameters():
     k = "poly"
     inducing_variables = 50
-    adam_iterations = 20000
+    adam_iterations = 5000
     minibatch_size = 100
     predict_func = lambda _, mean, var: mean
-
 
 def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
     """
@@ -203,9 +224,8 @@ def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
     visualization_xs = np.stack((grid_lon.flatten(), grid_lat.flatten()), axis=1)
 
     # Obtain predictions, means, and stddevs over the entire map
-    #predictions, \
-    gp_mean, gp_stddev = model.predict(visualization_xs)
-    #predictions = np.reshape(predictions, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
+    predictions, gp_mean, gp_stddev = model.predict(visualization_xs)
+    predictions = np.reshape(predictions, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
     gp_mean = np.reshape(gp_mean, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
     gp_stddev = np.reshape(gp_stddev, (EVALUATION_GRID_POINTS, EVALUATION_GRID_POINTS))
 
@@ -213,10 +233,10 @@ def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
     vmax_stddev = 35.5
 
     # Plot the actual predictions
-    #ax_predictions = fig.add_subplot(1, 3, 1)
-    #predictions_plot = ax_predictions.imshow(predictions, vmin=vmin, vmax=vmax)
-    #ax_predictions.set_title('Predictions')
-    #fig.colorbar(predictions_plot)
+    ax_predictions = fig.add_subplot(1, 3, 1)
+    predictions_plot = ax_predictions.imshow(predictions, vmin=vmin, vmax=vmax)
+    ax_predictions.set_title('Predictions')
+    fig.colorbar(predictions_plot)
 
     # Plot the raw GP predictions with their stddeviations
     ax_gp = fig.add_subplot(1, 3, 2, projection='3d')
@@ -247,21 +267,11 @@ def perform_extended_evaluation(model: Model, output_dir: str = '/results'):
     plt.show()
 
 
-
-
 def main():
     # Load the training dateset and test features
     train_x = np.loadtxt('train_x.csv', delimiter=',', skiprows=1)
     train_y = np.loadtxt('train_y.csv', delimiter=',', skiprows=1)
     test_x = np.loadtxt('test_x.csv', delimiter=',', skiprows=1)
-
-
-    # Plot X,Y,Z
-    #fig = plt.figure()
-    #ax = fig.add_subplot(111, projection='3d')
-    #ax.plot_trisurf(train_x[0:100,0], train_x[0:100,1], train_y[0:100], color='white', edgecolors='grey', alpha=0.5)
-    #ax.scatter(train_x[200:600,0], train_x[200:600,1], train_y[200:600], c='red')
-    #plt.show()
 
     # Fit the model
     print('Fitting model')
@@ -271,7 +281,10 @@ def main():
     # Predict on the test features
     print('Predicting on test features')
     predicted_y = model.predict(test_x)
-    print(predicted_y)
+    print('prediction ', predicted_y[:][0])
+    print('mean ', predicted_y[:][1])
+    print('variance ', predicted_y[1])
+
 
     if EXTENDED_EVALUATION:
         perform_extended_evaluation(model, output_dir='.')
@@ -279,5 +292,3 @@ def main():
 
 if __name__ == "__main__":
     main()
-
-
